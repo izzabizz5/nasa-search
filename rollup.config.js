46b6a94d71dfb2c139e1409e7f1da66b3ffc526c
izzabizz5 <154451126+izzabizz5@users.noreply.github.com>
2024-10-24 13:32:34 -0400

    Update rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,6 @@ import babel from '@rollup/plugin-babel';
 import { rollupPluginHTML as html } from '@web/rollup-plugin-html';
 import { importMetaAssets } from '@web/rollup-plugin-import-meta-assets';
 import esbuild from 'rollup-plugin-esbuild';
-import copy from 'rollup-plugin-copy';
 
 export default {
   input: 'index.html',
@@ -20,11 +19,6 @@ export default {
     /** Enable using HTML as rollup entrypoint */
     html({
       minify: true,
-    }),
-    copy({
-      targets: [
-        { src: 'lib/', dest: 'public' }
-      ]
     }),
     /** Resolve bare module imports */
     nodeResolve(),
